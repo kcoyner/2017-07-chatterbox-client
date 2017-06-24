89d390d4dc52c4175bab40c68cfea3cc96bcbb30
Kevin Coyner <rustybear@gmail.com>
2017-06-24 00:01:52 -0400

    doodle: Friday night capture.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -3,10 +3,27 @@ var app = {
   server: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages'
 };
 
-$(document).ready(function(){
-  $("#clear").on('click', function(event){
+$(document).ready(function() {
+  $('#clear').on('click', function(event) {
     app.clearMessages();
   });
+
+  $('#submitForm').on("submit",function(e) {
+    e.preventDefault(); // cancel the actual submit
+    var username = app.sanitize(window.location.search.replace("?username=", ''));
+    var text = app.sanitize($('#text').val());
+    var rooms = app.sanitize($('#rooms').val());
+
+    var message = {
+      username: username,
+      text: text,
+      roomname: rooms
+    };
+    console.log(message)
+    app.renderMessage(message);
+    // app.init();
+  });
+
 });
 
 // init method
@@ -14,16 +31,8 @@ app.init = function() {
   app.fetch();
 };
 
-
 // send method
 app.send = function(message) {
-
-  // var message = {
-  //   username: 'Mel Brooks',
-  //   text: 'It\'s good to be the king',
-  //   roomname: 'lobby'
-  // };
-
 
   $.ajax({
     // This is the url you should use to communicate with the parse API server.
@@ -39,40 +48,65 @@ app.send = function(message) {
       console.error('chatterbox: Failed to send message', data);
     }
   });
-}
+};
 
 // fetch method
 app.fetch = function() {
   $.ajax({
-      url: app.server,
-      type: 'GET',
-      contentType: 'application/json',
-      success: function (data) {
-      // console.log('chatterbox: Message received', data);
-      for (var i = 0 ; i < data.results.length; i++) {
+    url: app.server,
+    type: 'GET',
+    data: where={"order":"-createdAt","limit":"151"},
+    contentType: 'application/json',
+    success: function (data) {
+    console.log(data);
+      for (var i = 0; i < data.results.length; i++) {
         $('#chats').append('<div class="chat">' +
-                              '<div class="username">' + data.results[i].username + '</div>' +
-                              '<div class="text">' + data.results[i].text + '</div>' +
-                            '</div>');
+                            '<div class="username">' + app.sanitize(data.results[i].username) + '</div>' +
+                            '<div class="text">' + app.sanitize(data.results[i].text) + '</div>' +
+                            '<div class="text">' + app.sanitize(data.results[i].createdAt) + '</div>' +
+                          '</div>');
       }
-  },
-  error: function () {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to get message');
-  }
-});
+    },
+    error: function () {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get message');
+    }
+  });
+};
 
+// sanitize method
+app.sanitize = function (str) {
+
+  var entityMap = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;',
+    '/': '&#x2F;',
+    '`': '&#x60;',
+    '=': '&#x3D;'
+  };
+
+  return String(str).replace(/[&<>"'`=\/]/g, function fromEntityMap (s) {
+    return entityMap[s];
+  });
 }
 
+// clearMessages method
 app.clearMessages = function() {
   $('#chats').children().remove();
-}
+};
 
+// renderMessage method
 app.renderMessage = function(message) {
   //$('#chats').append(response);
   app.send(message);
-}
+};
 
+app.handleSubmit = function() {
+
+}
 app.init();
 
 
